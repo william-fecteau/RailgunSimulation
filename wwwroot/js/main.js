879c8c18e4a7fe76fcfafa1d7f8d98f8b90848ad
William Fecteau <william-fecteau@hotmail.com>
2021-11-06 17:45:26 -0400

    Fixed camera coordinate system

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -24,7 +24,7 @@ $(function() {
 ======================================
  */
 function update() {
-    cube.position.x += 0.01;
+    cube.position.x += 1;
 }
 
 
@@ -50,8 +50,7 @@ function setupGamefield() {
     gameField = $("#gamefield");
 
     scene = new THREE.Scene();
-    //camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera = new THREE.OrthographicCamera(window.innerWidth/-2, window.innerWidth/2, window.innerHeight/2, window.innerHeight/-2, -1, 1);
+    camera = new THREE.OrthographicCamera(0, window.innerWidth, window.innerHeight, 0, -1, 1);
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -100,6 +99,7 @@ $("#start-sim").click(function() {
     const geometry = new THREE.PlaneGeometry(100, 100);
     const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
     cube = new THREE.Mesh(geometry, material);
+    cube.position.set(100, 100, 0);
     scene.add(cube);
 
     isSimulationRunning = true;
